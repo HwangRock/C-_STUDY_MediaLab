e86d8030228f2cbf6a977035cefbb8b6b04657dc
김가을 <166911614+DARAMJll@users.noreply.github.com>
2024-07-15 21:45:52 +0900

    섹션 4 백준 문제풀이 김가을

---
+++ Section_4/문제풀이/BJ_1330_KimGaeul.cs
@@ -0,0 +1,31 @@
+﻿namespace Section4_BJ
+{
+	internal class BJ_1330_KimGaeul //두 수 
+	{
+		static void Main(string[] args)
+		{
+			string A, B, C;
+			C = Console.ReadLine();
+			A = C.Substring(0, 1);
+			B = C.Substring(2, 1);
+
+			int intA, intB;
+			intA = int.Parse(A);
+			intB = int.Parse(B);
+
+			if (intA > intB)
+			{
+				Console.WriteLine(">");
+			}
+			else if (intA < intB)
+			{
+				Console.WriteLine("<");
+			}
+			else
+			{
+				Console.WriteLine("==");
+			}
+
+		}
+	}
+}
---
+++ Section_4/문제풀이/BJ_2753_KimGaeul.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section4_BJ
+{
+	internal class BJ_2753_KimGaeul //윤년
+	{
+		static void Main(string[] args)
+		{
+			int year;
+			year = int.Parse(Console.ReadLine());
+
+			if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
+			{
+				Console.WriteLine("1");
+			}
+			else
+			{
+				Console.WriteLine("0");
+			}
+		}
+	}
+}

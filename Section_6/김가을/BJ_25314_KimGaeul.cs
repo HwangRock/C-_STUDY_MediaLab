bf00ce525f47ce5197a9772fb6e6b8f5b3b53cb9
김가을 <166911614+DARAMJll@users.noreply.github.com>
2024-07-18 17:01:31 +0900

    Update BJ_25314_KimGaeul.cs

--- Section_6/김가을/BJ_25314_KimGaeul.cs
+++ Section_6/김가을/BJ_25314_KimGaeul.cs
@@ -2,7 +2,7 @@
 			//문제 5.백준 25314 번 코딩은 체육과목입니다.
 			//문제 링크 : https://www.acmicpc.net/problem/25314
 
-      int n = int.Parse(Console.ReadLine());
+      			int n = int.Parse(Console.ReadLine());
 
 			while(n / 4 > 0) 
 			{

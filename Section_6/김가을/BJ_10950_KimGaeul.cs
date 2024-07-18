9adafd3c52a76e8f6054eb5f91fb644b4a8d02bb
김가을 <166911614+DARAMJll@users.noreply.github.com>
2024-07-18 16:21:44 +0900

    Create BJ_10950_KimGaeul.cs

---
+++ Section_6/김가을/BJ_10950_KimGaeul.cs
@@ -0,0 +1,14 @@
+      //문제 2.백준 10950 번 A +B
+			//문제 링크 : https://www.acmicpc.net/problem/10950
+
+      int i = int.Parse(Console.ReadLine());
+			int a, b;
+
+			while(i > 0)
+			{
+				string[] num = Console.ReadLine().Split();
+				a = int.Parse(num[0]);
+				b = int.Parse(num[1]);
+				Console.WriteLine(a + b);
+				i--;
+			}

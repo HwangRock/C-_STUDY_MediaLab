d8da20588a8a1c6faac53f382b099b1f5a8ec00f
김가을 <166911614+DARAMJll@users.noreply.github.com>
2024-07-18 16:46:35 +0900

    Update BJ_10950_KimGaeul.cs

--- Section_6/김가을/BJ_10950_KimGaeul.cs
+++ Section_6/김가을/BJ_10950_KimGaeul.cs
@@ -1,7 +1,7 @@
-      //문제 2.백준 10950 번 A +B
+      			//문제 2.백준 10950 번 A +B
 			//문제 링크 : https://www.acmicpc.net/problem/10950
 
-      int i = int.Parse(Console.ReadLine());
+      			int i = int.Parse(Console.ReadLine());
 			int a, b;
 
 			while(i > 0)

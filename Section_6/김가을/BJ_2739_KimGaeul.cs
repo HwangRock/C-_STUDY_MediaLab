03aa6d7ff7191d0fc15a362a9c441d59801959fe
김가을 <166911614+DARAMJll@users.noreply.github.com>
2024-07-18 16:13:39 +0900

    Update BJ_2739_KimGaeul.cs

--- Section_6/김가을/BJ_2739_KimGaeul.cs
+++ Section_6/김가을/BJ_2739_KimGaeul.cs
@@ -2,7 +2,7 @@
 //문제 링크 : https://www.acmicpc.net/problem/2739
 
 int n = int.Parse(Console.ReadLine());
-			for(int i = 1; i <= 9; i++)
-			{
-				Console.WriteLine(n + " * " + i + " = " + (n * i));
-			}
+for(int i = 1; i <= 9; i++)
+{
+	Console.WriteLine(n + " * " + i + " = " + (n * i));
+}

79b5337ae897b690fa7c4a54ced0e1689ed89623
김가을 <166911614+DARAMJll@users.noreply.github.com>
2024-07-22 15:52:51 +0900

    Update PG_괄호회전하기_김가을.cpp

--- Programmers/김가을/PG_괄호회전하기_김가을(미완).cpp
+++ Programmers/김가을/PG_괄호회전하기_김가을(미완).cpp
@@ -9,66 +9,66 @@ using namespace std;
 
 
 int main(int argc, char* argv[]) {
-    int answer = 0, count = 0;
-    bool status, a = true; //올바른 괄호인지?
+    int answer = 0;
+    bool status = false; //올바른 괄호인지?
     stack<char> stack;
     string s = "[](){}"; // ](){}[ (){}[] ){}[]( {}[]() }[](){ 
     int len = s.length();
     string tmp_s = "";
+    char top;
 
     //괄호 쌍 확인 : 스택
     //여는 괄호 : 스택에 push  / 닫는 괄호 : 스택 top 값과 비교
-    for (char c : s) {
-        switch(c){
-        case '(': case '[': case '{':
-            stack.push(c); break;
+        for (int i = 0; i < s.length(); i++) {
+            status = false;
+            cout << s << endl;
+        for (char c : s) {
+            top = ' ';
+            switch (c) {
+            case '(': case '[': case '{':
+                stack.push(c); break;
 
-        case ')': case ']': case '}':
-            if (stack.empty()) {
-                status = false;
-                a = false;
-            }
-            else { 
-                char top = stack.top();
-                stack.pop();
-                if ((c == ')' && top != '(') ||
-                    (c == ']' && top != '[') ||
-                    (c == '}' && top != '{')) {
+            case ')': case ']': case '}':
+                if (stack.empty()) {
                     status = false;
+                    break;
+                }
+                else {
+                    top = stack.top();
+                    
+                    if ((c == ')' && top == '(') ||
+                        (c == ']' && top == '[') ||
+                        (c == '}' && top == '{')) {
+                        stack.pop();
+                        status = true;
+                        break;
+                    }
+                    
                 }
             }
-        }
-        
-    }
 
-    //스택이 비어있으면 올바른 괄호
-    if (a) {
-        if (stack.empty()) { 
-            status = true;
+        }
+
+        //스택이 비어있으면 올바른 괄호
+        if (stack.empty() && status) {
             answer++;
         }
-        else {
-            status = false;
+        else if (!(stack.empty())) {
+            while (!(stack.empty())) {
+                stack.pop();
+            }
         }
-    }
-    
-    cout << answer << endl;
-
-
-    //괄호 회전 : 큐....안쓰고 걍 앞뒤글자만 바꾸기
-    
-    
-    while (count <= len ) {
-        char tmp = s.front();
-        for (int i = 0; i < len; i++) {
-            s[i] = s[i + 1];
-        }
-        s.back() = tmp;
-        count++;
-
-        cout << s << endl;
         
 
+
+        //괄호 회전 : 큐....안쓰고 걍 앞뒤글자만 바꾸기
+
+
+        char tmp = s.front();
+        s.erase(s.begin());
+        s.push_back(tmp);
+
+        
     }
 
     cout << answer << endl;
@@ -76,3 +76,100 @@ int main(int argc, char* argv[]) {
 
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////프로그래머스 제출///////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+/*
+#include <string>
+#include <vector>
+#include <stack>
+#include <iostream>
+
+using namespace std;
+
+int solution(string s) {
+    int answer = 0;
+bool status = false; //올바른 괄호인지?
+stack<char> stack;
+//string s = "[](){}"; // ](){}[ (){}[] ){}[]( {}[]() }[](){ 
+int len = s.length();
+string tmp_s = "";
+char top;
+
+//괄호 쌍 확인 : 스택
+//여는 괄호 : 스택에 push  / 닫는 괄호 : 스택 top 값과 비교
+    for (int i = 0; i < s.length(); i++) {
+        status = false;
+      //  cout << s << endl;
+    for (char c : s) {
+        top = ' ';
+        switch (c) {
+        case '(': case '[': case '{':
+            stack.push(c); break;
+
+        case ')': case ']': case '}':
+            if (stack.empty()) {
+                status = false;
+                break;
+            }
+            else {
+                top = stack.top();
+                
+                if ((c == ')' && top == '(') ||
+                    (c == ']' && top == '[') ||
+                    (c == '}' && top == '{')) {
+                    stack.pop();
+                    status = true;
+                    break;
+                }
+                
+            }
+        }
+
+    }
+
+    //스택이 비어있으면 올바른 괄호
+    if (stack.empty() && status) {
+        answer++;
+    }
+    else if (!(stack.empty())) {
+        while (!(stack.empty())) {
+            stack.pop();
+        }
+    }
+    
+
+
+    //괄호 회전 : 큐....안쓰고 걍 앞뒤글자만 바꾸기
+
+
+    char tmp = s.front();
+    s.erase(s.begin());
+    s.push_back(tmp);
+
+    
+}
+    return answer;
+}
+*/

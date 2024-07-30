1128cc5063cdce637e13cd1865b4d6715b78917f
HwangRock <157935545+HwangRock@users.noreply.github.com>
2024-07-30 14:47:54 +0900

    Update PG_자연수 뒤집어 배열로 만들기_황규민.cs

--- Programmers/황규민/PG_자연수 뒤집어 배열로 만들기_황규민.cs
+++ Programmers/황규민/PG_자연수 뒤집어 배열로 만들기_황규민.cs
@@ -9,6 +9,7 @@ public class Solution
         for (int i = 0; i < fin; i++)
         {
             answer[i] = s[i] - '0';
+            //int로 바꾸면 아스키코드값을 갖게된다. '0'은 48, '1'은 49 이런식으로. 이때 '1'에서 '0'을 빼면 그대로 1을 얻게 되는 방식으로 char에서 int로 변환.
         }
         Array.Reverse(answer);
         return answer;

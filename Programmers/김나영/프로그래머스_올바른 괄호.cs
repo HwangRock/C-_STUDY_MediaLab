c5cb79caca6f6a34810bf84cef223b0d01d5e746
kskdud <nykim0508@gmail.com>
2024-07-30 17:22:53 +0900

    Update 프로그래머스_올바른 괄호_김나영.cs

--- Programmers/김나영/프로그래머스_올바른 괄호.cs
+++ Programmers/김나영/프로그래머스_올바른 괄호.cs
@@ -1,25 +1,27 @@
-using System.Diagnostics.Tracing;
-using System.Runtime.ConstrainedExecution;
+using System;
+using System.Collections.Generic;
 
-namespace 올바른괄호
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            bool answer = true;
-            string s = Console.ReadLine();
-            for (int i = s[0]; i < args.Length; i+=2) //올바른 괄호라면 0포함 짝수계 다 "(" 테니까
-            {
-                if (i != "(")
-                { answer = false;
-                break}
-                return answer;
+public class Solution {
+    public bool solution(string s) {
+                       bool answer = true;
+               Stack<char> stacker = new Stack<char>();             
+               for (int i = 0; i < s.Length; i++)
+               {
+                   if (s[i] == '(')
+                   {
+                       stacker.Push(s[i]);
+                   }
+                   else
+                   { 
+                       if (stack.Count == 0)
+                       { answer = false;
+                       break;}
+                       stacker.Pop(); 
+                   }
 
-            }
-
-
-
-        }
+               }
+               if (stacker.Count > 0) 
+               { answer = false; }
+        return answer;
     }
 }

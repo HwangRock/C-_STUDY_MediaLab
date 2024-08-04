c96919e706e1c7cdd7afd9d23b125054600b413c
kskdud <nykim0508@gmail.com>
2024-08-04 22:22:19 +0900

    Create 문자열밀기_김나영.cs

---
+++ Programmers/김나영/문자열밀기_김나영.cs
@@ -0,0 +1,29 @@
+using System;
+
+public class Solution {
+    public int solution(string A, string B) {
+        int answer = 0;
+        int[] a = new int[] {};
+        
+        if (A==B)
+        {
+            answer = "0";
+        }
+        
+        for(int i=1; i<A.Length; i++)
+        {
+            a.Add(A[i]);
+        }      
+
+        if (a==B)
+        {
+            answer = "1";
+        }
+        else if (a!=B)
+        {
+            answer = "-1";
+        }
+        
+        return answer;
+    }
+}

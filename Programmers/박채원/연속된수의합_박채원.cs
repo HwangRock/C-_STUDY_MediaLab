1d6d1f9fc86a78254e7234fccda6638631f3a396
채원 <165558031+cwonni@users.noreply.github.com>
2024-08-05 11:00:02 +0900

    Create 연속된수의합_박채원.cs

---
+++ Programmers/박채원/연속된수의합_박채원.cs
@@ -0,0 +1,42 @@
+using System;
+
+public class Solution
+{
+    public int[] FindConsecutiveNumbers(int num, int total)
+    {
+        // 연속된 수열의 시작점 계산
+        int start = (total - (num * (num - 1) / 2)) / num;
+
+        // 연속된 수열 생성
+        int[] result = new int[num];
+        for (int i = 0; i < num; i++)
+        {
+            result[i] = start + i;
+        }
+
+        return result;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // 사용자로부터 입력 받기
+        Console.Write("Enter the number of consecutive integers (num): ");
+        int num = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter the total sum of these integers (total): ");
+        int total = int.Parse(Console.ReadLine());
+
+        // Solution 클래스의 인스턴스 생성
+        Solution solution = new Solution();
+        
+        // 연속된 정수 배열 찾기
+        int[] result = solution.FindConsecutiveNumbers(num, total);
+
+        // 결과 출력
+        Console.WriteLine("The consecutive numbers are:");
+        Console.WriteLine(string.Join(", ", result));
+    }
+}

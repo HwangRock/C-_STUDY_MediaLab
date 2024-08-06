1a8aa83e6333f2c2f327b6db786a3a956a1bd173
HwangRock <157935545+HwangRock@users.noreply.github.com>
2024-08-06 13:42:38 +0900

    Create PG_n개의최소공배수_황규민.cs

---
+++ Programmers/황규민/PG_n개의최소공배수_황규민.cs
@@ -0,0 +1,49 @@
+public class Solution
+{
+    int gcd(int n, int m)
+    {
+        if (m > n)
+        {
+            int temp = n;
+            n = m;
+            m = temp;
+        }
+        if (m == 0)
+        {
+            return n;
+        }
+
+        else
+        {
+            int r = n % m;
+            n = m;
+            m = r;
+            return gcd(n, m);
+        }
+    }
+
+    int lcm(int n, int m)
+    {
+        return (n * m) / gcd(n, m);
+    }
+
+    public int solution(int[] arr)
+    {
+        int answer = 0;
+        int fin = arr.Length;
+        if (fin == 1)
+        {
+            answer = arr[0];
+        }
+        else
+        {
+            int val = lcm(arr[0], arr[1]);
+            for(int i=2;i<fin; i++)
+            {
+                val=lcm(val, arr[i]);
+            }
+            answer = val;
+        }
+        return answer;
+    }
+}
